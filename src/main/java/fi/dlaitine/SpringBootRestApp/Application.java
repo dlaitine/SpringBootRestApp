e5519e238b24be53cc533526d21959e113a9eeec
dlaitine <dlaitine@gmail.com>
2018-03-03 11:42:32 +0200

    Add basic CRUD functionality and swagger

--- src/main/java/fi/dlaitine/SpringBootRestApp/Application.java
+++ src/main/java/fi/dlaitine/SpringBootRestApp/Application.java
@@ -1,8 +1,19 @@
 package fi.dlaitine.SpringBootRestApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EntityScan(
+        basePackageClasses = {Application.class, Jsr310JpaConverters.class}
+)
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
