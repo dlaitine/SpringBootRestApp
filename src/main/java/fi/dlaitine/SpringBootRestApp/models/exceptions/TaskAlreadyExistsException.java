abb5976025bf43481b3fc43ac857b2051894a5a7
dlaitine <dlaitine@gmail.com>
2018-09-17 13:53:27 +0300

    Add serialVersionUIDs

--- src/main/java/fi/dlaitine/SpringBootRestApp/models/exceptions/TaskAlreadyExistsException.java
+++ src/main/java/fi/dlaitine/SpringBootRestApp/models/exceptions/TaskAlreadyExistsException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value=HttpStatus.CONFLICT, reason="Task with same name already exists in database")
 public class TaskAlreadyExistsException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String name;
 	
 	public TaskAlreadyExistsException(String name) {
--- src/main/java/fi/dlaitine/SpringBootRestApp/models/exceptions/TaskNotFoundException.java
+++ src/main/java/fi/dlaitine/SpringBootRestApp/models/exceptions/TaskNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Task not found")
 public class TaskNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String name;
 	
 	public TaskNotFoundException(String name) {
